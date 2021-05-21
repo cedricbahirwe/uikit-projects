e9a532812deadee23964f26c94dd6f844f4077b4
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-22 00:07:01 +0200

    Passing data using prepare for segue function

--- ios-track/app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ ios-track/app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -14,13 +14,19 @@ class LeagueVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextBtn.isEnabled = false
         player = Player()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToSkillVC" {
+            if let vc = segue.destination as? SkillVC {
+                vc.player = player
+            }
+        }
+    }
     
     @IBAction func onNextTapped(_ sender: UIButton) {
-        
         performSegue(withIdentifier: "segueToSkillVC", sender: self)
     }
 
@@ -36,8 +42,8 @@ class LeagueVC: UIViewController {
         setLeague(to: "Coed")
     }
     
-    func setLeague(to league: String) {
-        player.desiredleague = league
+    func setLeague(to leagueType: String) {
+        player.desiredleague = leagueType
         nextBtn.isEnabled = true
     }
 }
